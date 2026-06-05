5b0920b77222e23afbe46e3b6143df6c8c4a7c19
Ahmad Kurniawan Ibrahim <151973627+ahwan18@users.noreply.github.com>
2026-06-05 13:29:00 +0700

    Update test-db.js

--- test-db.js
+++ test-db.js
@@ -1,7 +1,7 @@
 // import { createClient } from '@supabase/supabase-js';
 
 // const supabaseUrl = 'https://mreaatnxbfalhocpyide.supabase.co';
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yZWFhdG54YmZhbGhvY3B5aWRlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDUyNzExNCwiZXhwIjoyMDkwMTAzMTE0fQ.EcN7SLmA8_h3VuT2gtsz0gwLmxeHBcrFNL_0b3DIoTc';
+// const supabaseKey = '';
 // const supabase = createClient(supabaseUrl, supabaseKey);
 
 // async function checkDb() {
